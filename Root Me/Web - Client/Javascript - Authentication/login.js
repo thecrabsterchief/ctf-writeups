5b2d4f04fe6df3ec6673582351be722749d01374
idk-wh0am1 <viensea1106@gmail.com>
2023-07-18 12:15:58 +0700

    Add Root Me - Web Client: Javascript - Authentication

---
+++ Root Me/Web - Client/Javascript - Authentication/login.js
@@ -0,0 +1,14 @@
+/* <![CDATA[ */
+
+function Login(){
+	var pseudo=document.login.pseudo.value;
+	var username=pseudo.toLowerCase();
+	var password=document.login.password.value;
+	password=password.toLowerCase();
+	if (pseudo=="4dm1n" && password=="sh.org") {
+	    alert("Password accepté, vous pouvez valider le challenge avec ce mot de passe.\nYou an validate the challenge using this password.");
+	} else { 
+	    alert("Mauvais mot de passe / wrong password"); 
+	}
+}
+/* ]]> */ 
