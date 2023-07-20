ff639a3599d61268aa34fedd07b88a60182aeaf8
idk-wh0am1 <viensea1106@gmail.com>
2023-07-20 09:19:32 +0700

    Add dreamhack - Web: cookie

---
+++ dreamhack/Web/cookie/source/app.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+from flask import Flask, request, render_template, make_response, redirect, url_for
+
+app = Flask(__name__)
+
+try:
+    FLAG = open('./flag.txt', 'r').read()
+except:
+    FLAG = '[**FLAG**]'
+
+users = {
+    'guest': 'guest',
+    'admin': FLAG
+}
+
+@app.route('/')
+def index():
+    username = request.cookies.get('username', None)
+    if username:
+        return render_template('index.html', text=f'Hello {username}, {"flag is " + FLAG if username == "admin" else "you are not admin"}')
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return render_template('login.html')
+    elif request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        try:
+            pw = users[username]
+        except:
+            return '<script>alert("not found user");history.go(-1);</script>'
+        if pw == password:
+            resp = make_response(redirect(url_for('index')) )
+            resp.set_cookie('username', username)
+            return resp 
+        return '<script>alert("wrong password");history.go(-1);</script>'
+
+app.run(host='0.0.0.0', port=8000)
