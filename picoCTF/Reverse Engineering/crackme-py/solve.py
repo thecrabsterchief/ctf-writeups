c5857931cb7c18556db0a2365810921cc0b04966
viensea1106 <viensea1106@gmail.com>
2023-08-26 21:29:47 +0700

    add picoCTF - RE: crackme-py

---
+++ picoCTF/Reverse Engineering/crackme-py/solve.py
@@ -0,0 +1,27 @@
+bezos_cc_secret = "A:4@r%uL`M-^M0c0AbcM-MFE067d3eh2bN"
+
+# Reference alphabet
+alphabet = "!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ"+ \
+            "[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
+
+def decode_secret(secret):
+    """ROT47 decode
+
+    NOTE: encode and decode are the same operation in the ROT cipher family.
+    """
+
+    # Encryption key
+    rotate_const = 47
+
+    # Storage for decoded secret
+    decoded = ""
+
+    # decode loop
+    for c in secret:
+        index = alphabet.find(c)
+        original_index = (index + rotate_const) % len(alphabet)
+        decoded = decoded + alphabet[original_index]
+
+    print(decoded)
+
+decode_secret(bezos_cc_secret)
